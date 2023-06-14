d6b1554812182f550e08997bc07eb2455199957b
poojaaggarwal <paggarwal@dadco.com>
2023-06-14 10:27:38 -0600

    Alonso + Pooja API Changes

--- src/bowling.js
+++ src/bowling.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const bowling = {
     
     startBowling: function (){
@@ -12,6 +13,24 @@ const bowling = {
         knockedPins
     ) {
         return knockedPins;
+    },
+
+
+    async registerGame({frames,pins,rolls,test}) {
+
+        await axios.request({
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: 'http://pinsetter.herokuapp.com/pinsetter/?action=register&frames=10&pins=10&rolls=2&test=1-2-3-4-5-6-7-8-9/X12',
+            headers: { }
+        })
+            .then((response) => {
+                return response.data.id;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        return undefined;
     }
 }
 module.exports = bowling;
